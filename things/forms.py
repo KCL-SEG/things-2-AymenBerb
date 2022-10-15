8e9c5d4f86a9cebf1e9b52a3fe375fc8a1e63d11
Aymen Berbache <aymenberbache21@gmail.com>
2022-10-15 13:28:04 +0100

    Create ThingForm

--- things/forms.py
+++ things/forms.py
@@ -1,3 +1,12 @@
 """Forms of the project."""
+from django import forms
+from .models import Thing
+
 
 # Create your forms here.
+class ThingForm(forms.ModelForm):
+    
+    class Meta:
+        model = Thing
+        fields = ['name', 'description', 'quantity']
+        widgets = { 'description': forms.Textarea, 'quantity': forms.NumberInput} # A widget defines the display type of a HTML input element
